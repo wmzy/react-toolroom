59d5a4ef7e6fe16ee0d86759ccb5d64c489a5334
wmzy <1256573276@qq.com>
2023-05-31 22:26:57 +0800

    Updates

--- assets/index-a73f2289.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-0983955d.js";const s="ccompzo";function t({className:e}){return o.jsx("div",{className:e,children:"Hello World!"})}function n(){return o.jsxs("div",{className:"c19ojwk0",children:[o.jsx("h1",{children:"Welcome to React Toolroom"}),o.jsx(t,{className:s})]})}export{n as default};
---
+++ demos/assets/index-c57848bc.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-8beb3d20.js";const s="ccompzo";function t({className:e}){return o.jsx("div",{className:e,children:"Hello World!"})}function n(){return o.jsxs("div",{className:"c19ojwk0",children:[o.jsx("h1",{children:"Welcome to React Toolroom"}),o.jsx(t,{className:s})]})}export{n as default};
